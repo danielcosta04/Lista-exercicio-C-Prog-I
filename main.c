36659a7067c76c957ec7c3d864b9d47c9d2c31aa
Daniel Costa <124638683+danielcosta04@users.noreply.github.com>
2023-03-29 18:00:15 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,170 @@
+#include <stdio.h>
+
+int main(void) {
+  
+  //Lista 1 – Lógica, Condicionais e Repetições indefinidas
+
+/*1. Escreva um programa que leia duas variáveis inteiras e //troque o conteúdo entre elas.
+diaMesAno - camel case
+DiaMes - pascal case*/
+  
+ /* int a = 0;
+  int b = 0;
+  
+  printf("Digite a:\n");
+    scanf("%d", &a);
+  printf("Digite b:\n");
+    scanf("%d", &b);
+  printf("a: %d\tb: %d\n",a,b);
+
+  printf("b: %d\ta: %d\n",b,a);*/
+
+/*2. Ler um número inteiro e imprimir seu sucessor e seu antecessor.*/
+
+   /* int num1, sucessor, antecessor;
+    printf("Digite um número: ");
+      scanf("%d", &num1);
+      sucessor = num1 + 1;
+      antecessor = num1 - 1;
+    printf("O sucessor é: %d\n o antecessor é: %d", sucessor, antecessor);*/
+
+  /*3. Faça um programa que leia um valor de conta de restaurante, representando o gasto realizado pelo cliente e imprimir o valor total a ser pago, considerando que o restaurante cobra 10% de taxa de serviço do garçom.*/
+
+  /* float account = 0;
+  float fees = 0;
+  float accountTotal = 0;
+
+  printf("Digite o valor da conta: ");
+  scanf("%f", &account);
+  accountTotal = account * 1.10;
+  printf("O valor da conta do restaurante R$ %f", accountTotal);*/
+
+  /*4. Entrar com o raio de um círculo, calcular e imprimir seu perímetro e sua área. OBS: P = 2 * pi * raio e A = pi * raio * raio. Use pi = 3.14.*/
+
+  /*float perimeter = 0;
+  float area = 0;
+  float ray = 0;
+
+  printf("Digite o raio do círculo: ");
+  scanf("%f", &ray);
+  perimeter = 2 * 3.14 * ray;
+  area = 3.14 * ray * ray;
+
+  printf("O valor do perimetro %.2f", perimeter);
+  printf("\nO valor da área %.2f", area);*/
+  
+  /* 5. Ler uma temperatura em graus Centígrados e apresentá-la convertida em graus Fahrenheit. A fórmula de conversão é: F = (9*C+160)/5. Onde F é a temperatura em Fahrenheit e C é a temperatura em Centígrados.*/
+  
+  /*float celsius = 0;
+  float fahrenheit = 0;
+  
+  printf("Qual é a temperatura em Graus Celsius: ");
+  scanf("%f", &celsius);
+  fahrenheit = (9 * celsius + 160) / 5;
+
+  printf("A temperatura em fahrenheit é %.2f", fahrenheit);*/
+
+  /*6. Deseja-se calcular a conta de consumo de energia elétrica de um consumidor. Para isto, escreva um programa que leia o código do consumidor, o preço do KW e a quantidade de KW consumido, e exiba o código do consumidor e o total a pagar.
+- total a pagar = preço x quantidade
+- total a pagar mínimo = R$ 21,23*/
+
+ /* float codConumidor = 0;
+  float precoKw = 0;
+  float qtdKw = 0;
+  float totalPagar = 0;
+  float miniPagar = 0;
+
+  printf("Digite o código do consumidor: ");
+  scanf("%f", &codConumidor);
+  printf("Escreva o valor do KW: ");
+  scanf("%f", &precoKw);
+  printf("Quantos Kw foram consumidos: ");
+  scanf("%f", &qtdKw);
+  miniPagar = 21.23;
+  totalPagar = precoKw * qtdKw;
+  
+  if(totalPagar > miniPagar){
+    printf("O total a pagar é: %.2f", totalPagar);
+  }
+  else if(totalPagar < miniPagar){
+    printf("O total a pagar é: %.2f", miniPagar);
+  }
+  */
+  
+
+/*7. Receber dois números reais e imprimir na tela o maior entre eles. Se ambos forem iguais, deve ser exibida uma mensagem na tela.*/
+
+  /*float num1 = 0;
+  float num2 = 0;
+  printf("digite o primeiro número: ");
+  scanf("%f", &num1);
+  printf("Digite o segundo número: ");
+  scanf("%f", &num2);
+
+  if(num1 > num2){
+    printf("O número maior é o primeiro: %.2f", num1);
+  }else if(num1 < num2){
+    printf("O número maior é o segundo: %.2f", num2);
+  }else{
+    printf("Os números digitados são iguais ");
+  }*/
+
+/*8. Entrar com nota da AV1 e nota da AV2 de um aluno. Imprimir: nota de cada AV, média e uma das mensagens: AP se média >= 6, RP se média < 5 ou AV3 se media >=5 e media <6 (a média é 6.0 para aprovação).*/
+
+
+/*9. Ler três números a partir do teclado e apresentá-los em ordem decrescente.*/
+
+/*10. Faça um programa que simule um calculador de 4 operações. Seu programa deverá ler dois números e o operador e efetuar os cálculos, exibindo o resultado final. O programa deve ser capaz de verificar operações inválidas (no caso, divisão por zero), exibindo uma mensagem de alerta. DICA: o operador deve ser declarado como char. FAÇA USANDO SWITCH.*/
+  
+/*11. Desenvolver um algoritmo para calcular a conta de água. O custo da água varia dependendo se o consumidor é residencial, comercial ou industrial. A regra para calcular a conta é:
+• Residencial: R$5,00 de taxa mais R$0,05 por m3 gastos no total;
+• Comercial: R$500,00 para os primeiros 80 m3 gastos mais R$0,25 por m3 gastos além dos 80 m3;
+• Industrial: R$800,00 para os primeiros 100 m3 gastos mais R$0,04 por m3 gastos além dos 100 m3; O programa deverá ler a conta do cliente, o tipo de consumidor (residencial,
+
+comercial e industrial ) e o consumo de água por metros cúbicos. Como resultado, imprima a conta do cliente e o valor real a ser pago pelo mesmo.*/
+  
+/*12. Uma locadora de filmes tem a seguinte regra para aluguel de fitas:
+• Às segundas, terças e quintas (2, 3 e 5): um desconto de 40% em cima do preço normal; 
+• Às quartas, sextas, sábados e domingos (4, 6, 7 e 1): preço normal;
+• Aluguel de fitas comuns: preço normal
+• Aluguel de lançamentos: acréscimo de 15% em cima do preço normal. Desenvolver um algoritmo para ler o preço normal da fita (em R$), o dia da semana e a categoria do filme alugado (comum ou lançamento). Calcular e imprimir o preço final que será pago pela fita.
+FAÇA USANDO SWITCH.*/
+  
+/*13. Escrever um algoritmo que lê um conjunto de 4 valores “i”, “a”, “b” e “c”, onde “i” é um valor inteiro e positivo e “a” e “b” são quaisquer valores reais e os escreva a seguir:
+• Se i = 1 escrever os três valores a, b, c em ordem crescente;
+• Se i = 2 escrever os três valores a, b, c em ordem decrescente.
+• Se i = 3 escrever os três valores a, b, c de forma que o maior número entre a, b, c fique entre os outros dois.
+FAÇA USANDO SWITCH e IF*/
+  
+/*14. Desenvolver um algoritmo para calcular e imprimir o preço final de um carro. O valor do preço inicial de fábrica é fornecido por meio de entrada. O carro pode ter as seguintes opções:
+• (a) Ar condicionado: R$ 1750,00
+• (b) Pintura Metálica: R$ 800,00
+• (c) Vidro Elétrico: R$ 1200,00
+• (d) Direção Hidráulica: R$ 2000,00*/
+  
+/*15. Faça um algoritmo que leia tantos números quanto o usuário desejar e imprima a soma deles. Pare quando a entrada for 0.*/
+  
+/*16. Faça um programa que leia número reais maiores que zero. Quando for entrado o número zero, o programa deverá apresentar quantos números foram entrados e a média destes.*/ 
+  
+/*17. Faça um algoritmo que permita ao usuário informar a idade de quantas pessoas ele desejar. A entrada de idade 0 encerra a execução e mostra os resultados que são: a soma das pessoas maiores de idade e a média de idade das pessoas maiores de idade informadas.*/
+  
+/*18. Deseja-se fazer uma pesquisa a respeito do consumo mensal de energia elétrica em uma determinada cidade. Para isso, são fornecidos os seguintes dados:
+✓ preço do kWh consumido;
+✓ número do consumidor;
+✓ quantidade de kWh consumidos durante o mês;
+
+O número do consumidor igual a zero deve ser usado para interromper a entrada de  dados. Faça um programa que leia os dados descritos acima, calcule e imprima:
+
+(a) para cada consumidor o total a pagar,
+(b) o maior consumo verificado,
+(c) o menor consumo verificado,
+(d) a média geral de consumo.*/
+
+/*19. Faça um programa que mostre a tabuada de um número N (N será lido do teclado).*/
+  
+/*20. Faça um programa que implemente um menu onde o usuário deverá selecionar 0 ou 1. Ao entrar na opção 0, escreva na tela “Opção 0 escolhida”, exiba novamente o menu principal e permita que o usuário navegue novamente. O mesmo deve ser feito para a opção 1, alterando somente o texto para “Opção 1 escolhida”. Caso o usuário digite outro valor para a opção, saia do menu e escreva "Fim da execução".*/
+  
+  
+  
+  return 0;
+}
